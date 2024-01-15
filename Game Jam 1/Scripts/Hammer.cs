66ddd2a3010b774727a143d342993f68a7cdc1b2
Yash <91522919+YashDevLogs@users.noreply.github.com>
2024-01-15 13:32:43 +0530

    Add files via upload

---
+++ Game Jam 1/Scripts/Hammer.cs
@@ -0,0 +1,18 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour
+{
+    [SerializeField] private bool LeftRight;
+    void Start()
+    {
+        if (LeftRight)
+            transform.DORotate(new Vector3(0f, 0f, -90f), 1f).SetLoops(100000, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        else
+            transform.DORotate(new Vector3(0f, 0f, 90f), 1f).SetLoops(100000, LoopType.Yoyo).SetEase(Ease.InOutSine);
+
+    }
+    
+
+
+}
